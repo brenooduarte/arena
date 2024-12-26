3560fc1c0b2837fe7e31bc2e8b78771aad637c6e
Breno Duarte <brenoo.duarte@hotmail.com>
2024-12-26 13:48:23 -0300

    refactor: add import HttpSecurity

--- src/main/java/com/arena/domain/security/config/SecurityConfiguration.java
+++ src/main/java/com/arena/domain/security/config/SecurityConfiguration.java
@@ -3,16 +3,15 @@ package com.arena.domain.security.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @AllArgsConstructor
 public class SecurityConfiguration {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurityBeanDefinitionParser htpp) throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity htpp) throws Exception {
+        return null;
     }
 }
